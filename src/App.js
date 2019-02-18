51e226bd94b8630183fee43305b9e20ae5886005
hitha <hnandaku@hithas-mbp.corp.adobe.com>
2019-02-18 17:43:53 +0530

    second file change

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           First change in the file.
+          More changes in the file.
         </p>
       </div>
     );
